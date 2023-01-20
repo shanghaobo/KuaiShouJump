99ca7fa301f82762b06c23839716248155e46a53
shanghaobo <1186156343@qq.com>
2023-01-20 15:37:48 +0800

    init

---
+++ jump1.py
@@ -0,0 +1,87 @@
+"""
+自动识别兔子坐标版，考虑透视计算
+"""
+
+import pyautogui
+from pynput import keyboard
+import time
+from pynput.mouse import Button, Controller
+
+# 长按一秒跳跃的像素距离，越低跳的越远（可按实际修改配置）
+SECOND_DISTANCE = 528
+# 单次完成后延时秒数（可按实际修改配置）
+FINISHED_DELAY = 2
+
+# 不需要修改
+x1, y1 = 0, 0
+x2, y2 = 0, 0
+
+
+# 获取兔子坐标
+def get_person_point():
+    p = pyautogui.locateCenterOnScreen('rabbitRight.png', confidence=0.75)
+    if not p:
+        p = pyautogui.locateCenterOnScreen('rabbitLeft.png', confidence=0.75)
+    if not p:
+        return None, None
+    # 偏移调整
+    return p.x + 7, p.y + 69
+
+
+# 重置为默认
+def reset():
+    print('-----新一轮开始-----')
+    global x1
+    global y1
+    global x2
+    global y2
+    x1, y1 = 0, 0
+    x2, y2 = 0, 0
+
+    x1, y1 = get_person_point()
+    if not x1 and not y1:
+        print('未获取到兔子坐标')
+        print('等待1秒继续')
+        time.sleep(1)
+        reset()
+        return
+    print(f'已自动获取到兔子坐标：{x1},{y1}')
+    print('等待按右Ctrl标记跳跃地')
+
+
+# 执行跳一跳
+def run_jump(x1, y1, x2, y2):
+    if not all([x1, y1, x2, y2]):
+        return
+    # 计算距离
+    distance = ((x2 - x1) ** 2 + ((y2 - y1) * (2 ** 0.5)) ** 2) ** 0.5
+    print(f'距离:{distance}')
+    tm = round(distance / SECOND_DISTANCE, 3)
+    print(f'长按时间：{tm}秒')
+    print('开始自动执行')
+    mouse = Controller()
+    print('开始点击')
+    mouse.press(Button.left)
+    time.sleep(tm)
+    print('开始松手')
+    mouse.release(Button.left)
+    print('-----本次完成-----')
+    print(f'延时{FINISHED_DELAY}秒')
+    time.sleep(FINISHED_DELAY)
+    reset()
+
+
+# 监听按键
+def on_press(key):
+    if key == keyboard.Key.ctrl_r:
+        print('已按下右Ctrl')
+        global x2, y2
+        x2, y2 = pyautogui.position()
+        print(f'获取到坐标2：{x2},{y2}')
+        run_jump(x1, y1, x2, y2)
+
+
+if __name__ == "__main__":
+    reset()
+    with keyboard.Listener(on_press=on_press) as lsn:
+        lsn.join()
